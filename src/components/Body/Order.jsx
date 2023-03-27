78857d9dbe873b6a1861b15ebc0d53e0a2f6b654
marufbroh <marufmd775@gmail.com>
2023-03-27 20:20:44 +0600

    right order summay background added

--- src/components/Body/Order.jsx
+++ src/components/Body/Order.jsx
@@ -2,21 +2,25 @@ import React, { useEffect, useState } from 'react';
 import Product from '../Product/Product';
 
 const Order = () => {
-    const [products, useProducts] = useState([]);
+    const [products, setProducts] = useState([]);
     useEffect(() => {
-        fetch('/public/products.json')
+        fetch('products.json')
             .then(res => res.json())
-            .then(data => useProducts(data))
+            .then(data => setProducts(data))
     }, [])
+
+    const handleAddToCard = (ppp) => {
+        console.log(ppp)
+    }
     return (
-        <div className='flex flex-col lg:flex-row lg:justify-between lg:mx-12 gap-12'>
-            <div className='grid grid-cols-1 lg:grid-cols-3 gap-6 lg:basis-4/5 lg:my-24'>
+        <div className='flex flex-col lg:flex-row lg:justify-between'>
+            <div className='grid grid-cols-1 lg:grid-cols-3 gap-6 lg:basis-4/5 lg:m-24'>
                 {
-                    products.map((product) => <Product product={product} key={product.id} />)
+                    products.map((product) => <Product product={product} key={product.id} handleAddToCard={handleAddToCard} />)
                 }
             </div>
-            <div className='lg:basis-1/5'>
-                
+            <div className='lg:basis-1/5 h-screen bg-slate-700'>
+
             </div>
         </div>
     );
--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 const Product = (props) => {
     // console.log(props.product);
-    const { name, price, img, seller, ratings } = props.product
+    const { name, price, img, seller, ratings, quantity } = props.product;
+    const handleAddToCard = props.handleAddToCard;
     return (
-        <div className="card bg-base-100 shadow-xl">
+        <div className="card bg-base-100 shadow-2xl">
             <figure><img src={img} alt="Shoes" /></figure>
             <div className="card-body">
                 <h2 className="card-title">{name}</h2>
@@ -13,10 +14,11 @@ const Product = (props) => {
                     <p>Manufacturer: {seller}</p>
                     <p>Rating: {ratings} Star</p>
                 </div>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
-                </div>
             </div>
+            <button onClick={() => handleAddToCard(name)} className="btn btn-block" style={{ backgroundColor: '#1C2B35' }}>Add to Cart <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 ml-2">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+            </svg>
+            </button>
         </div>
     );
 };
