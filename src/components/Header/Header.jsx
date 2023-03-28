ff1333c7d2994ee345cb0581ad40e395ad13de48
marufbroh <marufmd775@gmail.com>
2023-03-28 06:47:16 +0600

    header responsive fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,15 +7,14 @@ import logo from '../../images/Logo.svg'
 
 const Header = () => {
     return (
-        <nav className='flex justify-between py-4 lg:px-32 items-center' style={{ backgroundColor: '#1C2B35' }}>
+        <nav className='flex justify-between py-4 px-4 lg:px-32 items-center' style={{ backgroundColor: '#1C2B35' }}>
             <img src={logo} alt="" />
-            <div className='text-white flex gap-6'>
+            <div className='text-white flex flex-col lg:flex-row lg:gap-6'>
                 <a href='/order'>Order</a>
                 <a href='/order-review'>Order Review</a>
                 <a href='/manage-inventory'>Manage Inventory</a>
                 <a href='/login'>Login</a>
             </div>
-
         </nav>
     );
 };
