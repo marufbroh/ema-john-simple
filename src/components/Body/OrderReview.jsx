bcdd753f96bdf6ae33e265bbbc1b2bf3f17c9bc9
marufbroh <marufmd775@gmail.com>
2023-04-05 17:57:50 +0600

    order review cart added

--- src/components/Body/OrderReview.jsx
+++ src/components/Body/OrderReview.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import OrderSummary from '../Order-Summary/OrderSummary';
 import { useLoaderData } from 'react-router-dom';
+import ReviewItems from '../ReviewItems/ReviewItems';
 
 
 const OrderReview = () => {
@@ -8,12 +9,14 @@ const OrderReview = () => {
     // console.log(cart);
     // handleClearCart={handleClearCart}
     return (
-        <div className='flex flex-col gap-6 lg:flex-row lg:justify-between container mx-auto lg:py-28'>
+        <div className='flex flex-col gap-6 lg:flex-row lg:justify-between lg:items-center container mx-auto lg:py-28'>
             <div className='flex flex-col gap-6 lg:basis-3/5'>
-                <p>cart number is {cart.length}</p>
+                {
+                    cart.map(item => <ReviewItems key={item.id} item={item} />)
+                }
             </div>
-            <div className='lg:basis-2/5 bg-slate-700 text-white'>
-                <OrderSummary cart={cart}  />
+            <div className='lg:basis-2/5 bg-slate-700 text-white rounded-xl'>
+                <OrderSummary cart={cart} />
 
             </div>
         </div>
