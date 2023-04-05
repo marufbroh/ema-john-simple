0b9922b902646cc3a49c76c8de96095954b31a4c
marufbroh <marufmd775@gmail.com>
2023-04-05 13:12:42 +0600

    nasted route and Navlink added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,52 @@
 import React from 'react';
-import Login from '../Body/Login';
-import ManageInventory from '../Body/ManageInventory';
-import Order from '../Body/Order';
-import OrderReview from '../Body/OrderReview';
 import logo from '../../images/Logo.svg'
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
         <nav className='flex justify-between py-4 px-4 lg:px-32 items-center' style={{ backgroundColor: '#1C2B35' }}>
             <img src={logo} alt="" />
             <div className='text-white flex flex-col lg:flex-row lg:gap-6'>
-                <a href='/order'>Order</a>
-                <a href='/order-review'>Order Review</a>
-                <a href='/manage-inventory'>Manage Inventory</a>
-                <a href='/login'>Login</a>
+                <NavLink
+                    to={"/"}
+                    className={({ isActive }) =>
+                        isActive
+                            ? "text-yellow-500"
+                            : ""
+                    }
+                >
+                    Order
+                </NavLink>
+                <NavLink
+                    to={"/order-review"}
+                    className={({ isActive }) =>
+                        isActive
+                            ? "text-yellow-500"
+                            : ""
+                    }
+                >
+                    Order Review
+                </NavLink>
+                <NavLink
+                    to={"/manage-inventory"}
+                    className={({ isActive }) =>
+                        isActive
+                            ? "text-yellow-500"
+                            : ""
+                    }
+                >
+                    Manage Inventory
+                </NavLink>
+                <NavLink
+                    to={"/login"}
+                    className={({ isActive }) =>
+                        isActive
+                            ? "text-yellow-500"
+                            : ""
+                    }
+                >
+                    Login
+                </NavLink>
             </div>
         </nav>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,42 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Order from './components/Body/Order';
+import OrderReview from './components/Body/OrderReview';
+import ManageInventory from './components/Body/ManageInventory';
+import Login from './components/Body/Login';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Order />
+      },
+      {
+        path: 'order-review',
+        element: <OrderReview />
+      },
+      {
+        path: 'manage-inventory',
+        element: <ManageInventory />
+      },
+      {
+        path: 'login',
+        element: <Login />
+      },
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
